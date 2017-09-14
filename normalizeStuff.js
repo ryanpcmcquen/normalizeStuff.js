b88bb08c17b833a8a48098b2771257161aa0414b
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-09-14 09:12:58 -0700

    0.3.0

--- normalizeStuff.js
+++ normalizeStuff.js
@@ -1,57 +1,55 @@
-/*! normalizeStuff.js v0.2.1 by ryanpcmcquen */
-/*jslint browser:true, white:true*/
+/*! normalizeStuff.js v0.3.0 by ryanpcmcquen */
+/*jslint browser:true*/
 /*global window*/
 (function () {
 
-  "use strict";
+    "use strict";
 
-  var normalizeStuff = {};
+    var normalizeStuff = {};
 
-  normalizeStuff.heights = function (elementSelector, height) {
-    var elementArray = Array.prototype.slice.call(document.querySelectorAll(elementSelector));
-    var maxElementHeight;
-    if (!height) {
-      // no height was defined, calculate tallest height
-      var elementHeightArray = [];
-      elementArray.map(function (i) {
-        elementHeightArray.push(i.clientHeight);
-      });
+    normalizeStuff.heights = function (elementSelector, height) {
+        var elementArray = Array.prototype.slice.call(document.querySelectorAll(elementSelector));
+        var maxElementHeight;
+        if (!height) {
+            // No height was defined, calculate tallest height.
+            var elementHeightArray = elementArray.map(function (i) {
+                return i.clientHeight;
+            });
 
-      // get the tallest height
-      maxElementHeight = Math.max.apply(Math, elementHeightArray);
-    } else {
-      // specific height defined
-      maxElementHeight = height;
-    }
-    // apply that height to all elements
-    elementArray.map(function (i) {
-      i.style.height = maxElementHeight + "px";
-    });
-  };
+            // Get the tallest height.
+            maxElementHeight = Math.max.apply(Math, elementHeightArray);
+        } else {
+            // Specific height defined.
+            maxElementHeight = height;
+        }
+        // Apply that height to all elements.
+        elementArray.forEach(function (i) {
+            i.style.height = maxElementHeight + "px";
+        });
+    };
 
-  normalizeStuff.widths = function (elementSelector, width) {
-    var elementArray = Array.prototype.slice.call(document.querySelectorAll(elementSelector));
-    var maxElementWidth;
-    if (!width) {
-      // no width was defined, calculate greatest width
-      var elementWidthArray = [];
-      elementArray.map(function (i) {
-        elementWidthArray.push(i.clientWidth);
-      });
+    normalizeStuff.widths = function (elementSelector, width) {
+        var elementArray = Array.prototype.slice.call(document.querySelectorAll(elementSelector));
+        var maxElementWidth;
+        if (!width) {
+            // No width was defined, calculate greatest width.
+            var elementWidthArray = elementArray.map(function (i) {
+                return i.clientWidth;
+            });
 
-      // get the greatest width
-      maxElementWidth = Math.max.apply(Math, elementWidthArray);
-    } else {
-      // specific width defined
-      maxElementWidth = width;
-    }
-    // apply that width to all elements
-    elementArray.map(function (i) {
-      i.style.width = maxElementWidth + "px";
-    });
-  };
+            // Get the greatest width.
+            maxElementWidth = Math.max.apply(Math, elementWidthArray);
+        } else {
+            // Specific width defined.
+            maxElementWidth = width;
+        }
+        // Apply that width to all elements.
+        elementArray.forEach(function (i) {
+            i.style.width = maxElementWidth + "px";
+        });
+    };
 
-  // attach globally
-  window.normalizeStuff = normalizeStuff;
+    // Attach globally.
+    window.normalizeStuff = normalizeStuff;
 
 }());
